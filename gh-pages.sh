06c24ef2193de72ad5112621a22b68822c1035b3
yiminghe <yiminghe@gmail.com>
2015-03-17 18:25:50 +0800

    clean webpack

--- gh-pages.sh
+++
@@ -1,9 +0,0 @@
-npm run less
-npm run example
-cd ../form-validation-gh-pages
-rm -rf build/
-mkdir build
-cp -r ../form-validation/build/ build
-git add --all
-git commit -am "update examples"
-git push origin gh-pages:gh-pages
